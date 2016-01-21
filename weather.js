5a3c340bff84bbd99254b2b17e4f1d7bb87cccaf
vargo032 <o-vargas@kellogg.northwestern.edu>
2016-01-20 20:45:41 -0600

    created week 3 hw
    repository

---
+++ weather.js
@@ -0,0 +1,26 @@
+// Dollar sign is just another way of saying jQuery function
+$(function() {
+  var handleWeatherResponse = function(data) {
+    console.log(data);
+
+    // Put your code here to change the "markup" variable.
+    // Don't change any other code in this file. You will be sad.
+
+    var markup = "The weather report... " + "will be here when I finish my homework, homie.";
+
+
+    // End of your code
+
+    $('.weather-report').html(markup);
+  }
+  $('a.get-the-weather').on('click', function(event) {
+    event.preventDefault();
+    $.ajax({
+      type: 'GET',
+      url: 'https://api.forecast.io/forecast/6dbe98374cc5b8f9ea63d5ec73de9c04/42.056459,-87.675267?callback=?',
+      dataType: 'jsonp',
+      contentType: "application/json",
+      success: handleWeatherResponse
+    });
+  });
+});
